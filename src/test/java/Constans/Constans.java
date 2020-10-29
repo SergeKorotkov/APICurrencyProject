ace38cf204a2869d5475df8514c6475fb0b0a780
Serge <korotkov@post.com>
2020-10-28 20:07:21 -0400

    New version. Added constans.

---
+++ src/test/java/Constans/Constans.java
@@ -0,0 +1,20 @@
+package Constans;
+
+public class Constans {
+    public static String ROOT = "http://api.currencylayer.com/live?access_key=";
+    public static String HISTORICAL_ROOT = "http://api.currencylayer.com/historical?access_key=";
+    public static String KEY = "240f6de08f01dd4943b2cb587736374f";
+    public static String CAD_QUOTE = "quotes.USDCAD";
+    public static String EUR_QUOTE = "quotes.USDEUR";
+    public static String ILS_QUOTE = "quotes.USDILS";
+    public static String RUB_QUOTE = "quotes.USDRUB";
+    public static String CURRENCY_QUOTE = "&currencies=CAD,EUR,ILS,RUB";
+    public static String DATE_QUOTE = "&Date=2018-01-27";
+    public static String ERROR_CODE = "error.code";
+    public static String ERROR_TYPE = "error.type";
+    public static String ERROR_INFO = "error.info";
+    public static String SUCCESS = "success";
+    public static String PRIVACY = "privacy";
+    public static String TIMESTAMP = "timestamp";
+    public static String SOURCE = "source";
+}
--- src/test/java/CurrencyAPITest.java
+++ src/test/java/CurrencyAPITest.java
@@ -11,59 +11,58 @@ import java.util.ArrayList;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static Constans.Constans.*;
+
 
 public class CurrencyAPITest {
-
-    final static String ROOT = "http://api.currencylayer.com/live?access_key=";
-    final static String HISTORICAL_ROOT = "http://api.currencylayer.com/historical?access_key=";
-    final static String KEY = "240f6de08f01dd4943b2cb587736374f";
-
 
     @Test
     public void getActualCurrencyForOneTest() {
         Response response = given().get(ROOT + KEY + "&currencies=CAD");
-        response.then().body("success", notNullValue());
-        response.then().body("privacy", notNullValue());
-        response.then().body("timestamp", notNullValue());
-        response.then().body("source", notNullValue());
-        response.then().body("quotes.USDCAD", notNullValue());
+        response.then().body(SUCCESS, notNullValue());
+        response.then().body(PRIVACY, notNullValue());
+        response.then().body(TIMESTAMP, notNullValue());
+        response.then().body(SOURCE, notNullValue());
+        response.then().body(CAD_QUOTE, notNullValue());
         response.then().log().body();
     }
+
     @Test
     public void getHistoricalCurrencyForOneTest() {
-        Response response = given().get(HISTORICAL_ROOT + KEY + "&Date=2018-01-27&currencies=CAD");
-        response.then().body("success", notNullValue());
-        response.then().body("privacy", notNullValue());
-        response.then().body("timestamp", notNullValue());
-        response.then().body("source", notNullValue());
-        response.then().body("quotes.USDCAD", notNullValue());
+        Response response = given().get(HISTORICAL_ROOT + KEY + DATE_QUOTE+"&currencies=CAD");
+        response.then().body(SUCCESS, notNullValue());
+        response.then().body(PRIVACY, notNullValue());
+        response.then().body(TIMESTAMP, notNullValue());
+        response.then().body(SOURCE, notNullValue());
+        response.then().body(CAD_QUOTE, greaterThan(0f));
         response.then().log().body();
     }
+
     @Test
     public void getActualCurrencyTest() {
-        Response response = given().get(ROOT + KEY + "&currencies=CAD,EUR,ILS,RUB");
-        response.then().body("success", notNullValue());
-        response.then().body("privacy", notNullValue());
-        response.then().body("timestamp", notNullValue());
-        response.then().body("source", notNullValue());
-        response.then().body("quotes.USDCAD", notNullValue());
-        response.then().body("quotes.USDEUR", notNullValue());
-        response.then().body("quotes.USDILS", notNullValue());
-        response.then().body("quotes.USDRUB", notNullValue());
+        Response response = given().get(ROOT + KEY + CURRENCY_QUOTE);
+        response.then().body(SUCCESS, notNullValue());
+        response.then().body(PRIVACY, notNullValue());
+        response.then().body(TIMESTAMP, notNullValue());
+        response.then().body(SOURCE, notNullValue());
+        response.then().body(CAD_QUOTE, greaterThan(0f));
+        response.then().body(EUR_QUOTE, greaterThan(0f));
+        response.then().body(ILS_QUOTE, greaterThan(0f));
+        response.then().body(RUB_QUOTE, greaterThan(0f));
         response.then().log().body();
     }
 
     @Test
     public void getHistoricalCurrencyTest() {
-        Response response = given().get(HISTORICAL_ROOT + KEY + "&Date=2018-01-27&currencies=CAD,EUR,ILS,RUB");
-        response.then().body("success", notNullValue());
-        response.then().body("privacy", notNullValue());
-        response.then().body("timestamp", notNullValue());
-        response.then().body("source", notNullValue());
-        response.then().body("quotes.USDCAD", notNullValue());
-        response.then().body("quotes.USDEUR", notNullValue());
-        response.then().body("quotes.USDILS", notNullValue());
-        response.then().body("quotes.USDRUB", notNullValue());
+        Response response = given().get(HISTORICAL_ROOT + KEY + DATE_QUOTE+CURRENCY_QUOTE);
+        response.then().body(SUCCESS, notNullValue());
+        response.then().body(PRIVACY, notNullValue());
+        response.then().body(TIMESTAMP, notNullValue());
+        response.then().body(SOURCE, notNullValue());
+        response.then().body(CAD_QUOTE, greaterThan(0f));
+        response.then().body(EUR_QUOTE, greaterThan(0f));
+        response.then().body(ILS_QUOTE, greaterThan(0f));
+        response.then().body(RUB_QUOTE, greaterThan(0f));
         response.then().body("date", equalTo("2018-01-27"));
         response.then().log().body();
     }
@@ -74,91 +73,98 @@ public class CurrencyAPITest {
         response.then().log().body();
         response.then().statusCode(200);
     }
+
     @Test
     public void historicalDateValidKeyTest() {
-        Response response = given().get(HISTORICAL_ROOT + KEY+"&Date=2018-01-28");
+        Response response = given().get(HISTORICAL_ROOT + KEY + DATE_QUOTE);
         response.then().log().body();
         response.then().statusCode(200);
-        response.then().body("historical",equalTo(true));
+        response.then().body("historical", equalTo(true));
     }
+
     @Test
     public void actualDateNotValidKeyTest() {
-        Response response = given().get(ROOT + "&Date=2018-01-27&currencies=CAD,EUR,ILS,RUB");
+        Response response = given().get(ROOT + DATE_QUOTE+CURRENCY_QUOTE);
         response.then().log().body();
-        response.then().body("error.code", equalTo(101));
-        response.then().body("error.type", equalTo("missing_access_key"));
-        response.then().body("error.info", equalTo("You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]"));
+        response.then().body(ERROR_CODE, equalTo(101));
+        response.then().body(ERROR_TYPE, equalTo("missing_access_key"));
+        response.then().body(ERROR_INFO, equalTo("You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]"));
     }
+
     @Test
     public void historicalDateNotValidKeyTest() {
-        Response response = given().get(HISTORICAL_ROOT + "&Date=2018-01-27&currencies=CAD,EUR,ILS,RUB");
+        Response response = given().get(HISTORICAL_ROOT + DATE_QUOTE+CURRENCY_QUOTE);
         response.then().log().body();
-        response.then().body("error.code", equalTo(101));
-        response.then().body("error.type", equalTo("missing_access_key"));
-        response.then().body("error.info", equalTo("You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]"));
+        response.then().body(ERROR_CODE, equalTo(101));
+        response.then().body(ERROR_TYPE, equalTo("missing_access_key"));
+        response.then().body(ERROR_INFO, equalTo("You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]"));
     }
 
     @Test
     public void getStatusCode101Test() {
         Response response = given().get(ROOT + "Not valid key");
         response.then().log().body();
-        response.then().body("error.code", equalTo(101));
-        response.then().body("error.type", equalTo("invalid_access_key"));
+        response.then().body(ERROR_CODE, equalTo(101));
+        response.then().body(ERROR_TYPE, equalTo("invalid_access_key"));
     }
+
     @Test
     public void getStatusCode103Test() {
-        Response response = given().get("http://api.currencylayer.com/liver?access_key=" + KEY);
+        Response response = given().get("http://api.currencylayer.com/liver?access_key="+KEY);
         response.then().log().body();
-        response.then().body("error.code", equalTo(103));
-        response.then().body("error.info", equalTo("This API Function does not exist."));
+        response.then().body(ERROR_CODE, equalTo(103));
+        response.then().body(ERROR_INFO, equalTo("This API Function does not exist."));
     }
+
     @Test
     public void getStatusCode105Test() {
-        Response response = given().get(HISTORICAL_ROOT+ KEY +"&Date=1995-01-01"+"&source=CAD");
+        Response response = given().get(HISTORICAL_ROOT + KEY + "&Date=1995-01-01" + "&source=CAD");
         response.then().log().body();
-        response.then().body("error.code", equalTo(105));
-        response.then().body("success", equalTo(false));
-        response.then().body("error.info", equalTo("Access Restricted - Your current Subscription Plan does not support Source Currency Switching."));
+        response.then().body(ERROR_CODE, equalTo(105));
+        response.then().body(SUCCESS, equalTo(false));
+        response.then().body(ERROR_INFO, equalTo("Access Restricted - Your current Subscription Plan does not support Source Currency Switching."));
     }
+
     @Test
     public void getStatusCode106Test() {
-        Response response = given().get(HISTORICAL_ROOT+ KEY +"&Date=1995-01-01");
+        Response response = given().get(HISTORICAL_ROOT + KEY + "&Date=1995-01-01");
         response.then().log().body();
-        response.then().body("error.code", equalTo(106));
-        response.then().body("success", equalTo(false));
-        response.then().body("error.info", equalTo("Your query did not return any results. Please try again."));
+        response.then().body(ERROR_CODE, equalTo(106));
+        response.then().body(SUCCESS, equalTo(false));
+        response.then().body(ERROR_INFO, equalTo("Your query did not return any results. Please try again."));
     }
+
     @Test
     public void getStatusCode201Test() {
-        Response response = given().get(HISTORICAL_ROOT+ KEY +"&Date=1995-01-01"+"&source=CADA");
+        Response response = given().get(HISTORICAL_ROOT + KEY + DATE_QUOTE+ "&source=CADA");
         response.then().log().body();
-        response.then().body("error.code", equalTo(201));
-        response.then().body("success", equalTo(false));
-        response.then().body("error.info", equalTo("You have supplied an invalid Source Currency. [Example: source=EUR]"));
+        response.then().body(ERROR_CODE, equalTo(201));
+        response.then().body(SUCCESS, equalTo(false));
+        response.then().body(ERROR_INFO, equalTo("You have supplied an invalid Source Currency. [Example: source=EUR]"));
     }
 
     @Test
     public void getStatusCode202Test() {
         Response response = given().get(ROOT + KEY + "&currencies=Not correct currency");
         response.then().log().body();
-        response.then().body("error.code", equalTo(202));
-        response.then().body("error.info", equalTo("You have provided one or more invalid Currency Codes. [Required format: currencies=EUR,USD,GBP,...]"));
+        response.then().body(ERROR_CODE, equalTo(202));
+        response.then().body(ERROR_INFO, equalTo("You have provided one or more invalid Currency Codes. [Required format: currencies=EUR,USD,GBP,...]"));
     }
 
     @Test
     public void getStatusCode301Test() {
         Response response = given().get(HISTORICAL_ROOT + KEY + "&Date=");
         response.then().log().body();
-        response.then().body("error.code", equalTo(301));
-        response.then().body("error.info", equalTo("You have not specified a date. [Required format: date=YYYY-MM-DD]"));
+        response.then().body(ERROR_CODE, equalTo(301));
+        response.then().body(ERROR_INFO, equalTo("You have not specified a date. [Required format: date=YYYY-MM-DD]"));
     }
 
     @Test
     public void getStatusCode302Test() {
         Response response = given().get(HISTORICAL_ROOT + KEY + "&Date=2020-27-27");
         response.then().log().body();
-        response.then().body("error.code", equalTo(302));
-        response.then().body("error.info", equalTo("You have entered an invalid date. [Required format: date=YYYY-MM-DD]"));
+        response.then().body(ERROR_CODE, equalTo(302));
+        response.then().body(ERROR_INFO, equalTo("You have entered an invalid date. [Required format: date=YYYY-MM-DD]"));
     }
 
 
